6485f264b7c9ffd389f6f965a60fc9b8ccb0ba0b
MFaisalQasim <77318032+MFaisalQasim@users.noreply.github.com>
2024-09-16 13:10:20 -0700

    chat-box created
    chat-box created

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Chatpage
+-   Clone or download this repo and place it into your server.
+-   `composer install `
+-   `cp .env.example .env `
+-   Create database and modify .env with your DB name and Pusher credentials.
+-   `php artisan migrate --seed`
+-   `php artisan key:generate`
+-   `npm install && npm run dev`
+-   `php artisan storage:link`
+-   `php artisan serve`
+
+then choose a user from the database and login.
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,54 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+<style>
+
+/* CSS */
+.button-1 {
+  background-color: #EA4C89;
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 0;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: color 100ms;
+  vertical-align: baseline;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-1:hover,
+.button-1:focus {
+  background-color: #F082AC;
+}
+</style>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+    <!-- HTML !-->
+            <a class="button-1" href="/chatify" role="button">Chat Here</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
